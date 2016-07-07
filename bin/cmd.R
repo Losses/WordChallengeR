8a1856340db0bc0197af0a0cf4209e60c51b5bf5
Losses <losses.don@gmail.com>
2016-07-07 20:05:00 +0800

    add reset function for debug.

--- bin/cmd.R
+++ bin/cmd.R
@@ -1,13 +1,20 @@
-rm(list = ls())
-Sys.setlocale("LC_CTYPE","chinese")
-Sys.setenv(LANG = "en")
-
-for (.lib_file in list.files('bin/lib')) {
-  cat(sprintf('Loading Library: %s\n', .lib_file))
-  source(sprintf('bin/lib/%s',.lib_file), encoding = 'utf8')
+init <- function(){
+  rm(list = ls())
+  Sys.setlocale("LC_CTYPE","chinese")
+  Sys.setenv(LANG = "en")
+  
+  for (.lib_file in list.files('bin/lib')) {
+    cat(sprintf('Loading Library: %s\n', .lib_file))
+    source(sprintf('bin/lib/%s',.lib_file), encoding = 'utf8')
+  }
+  
+  standard_lib <<- readLines('etc/standardLib.txt')
+  WORD_CACHE <<- build_word_cache()
 }
 
-standard_lib <- readLines('etc/standardLib.txt')
-WORD_CACHE <- build_word_cache()
+reset <- function(){
+  init()
+  wcR()
+}
 
-wdR()
+reset()
