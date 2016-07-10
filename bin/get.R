208fce969f20c41fe92558429dccaa64233a47b4
Losses <losses.don@gmail.com>
2016-07-10 19:26:07 +0800

    code beautify.

--- bin/get.R
+++ bin/get.R
@@ -1,8 +1,11 @@
-cat(
-  as.dictionary(
-    read.word(
-      paste(LAST_COMMAND[-1], collapse = ' ')
-      )
-    )
-  )
+#--------------------------------------------------------
+#  DICTIONARY READER
+#  By Losses Don @ 2016-07-10
+#--------------------------------------------------------
+#
+# command:
+#   get [word]
+#
+
+paste(LAST_COMMAND[-1], collapse = ' ') %>% read.word %>% as.dictionary %>% cat
 cat('\n')
