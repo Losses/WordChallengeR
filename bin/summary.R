14368c852a3783b4a487346cfa77dab128c18dfb
Losses Don <losses.don@gmail.com>
2016-07-31 20:27:19 +0800

    Beautify summary tool.

--- bin/summary.R
+++ bin/summary.R
@@ -1,3 +1,15 @@
-cat(sprintf('Summary\n-------\nTotal entries\t%s\nIn scope\t%s\nPharses     \t%s\nOut of scope\t%s\n',
-            nrow(WORD_CACHE), sum(WORD_CACHE$in_lib) - sum(sapply(WORD_CACHE$word, is.pharse)),
-            sum(sapply(WORD_CACHE$word, is.pharse)), sum(!WORD_CACHE$in_lib)))
+#--------------------------------------------------------
+#  SUMMARY TOOL
+#  By Losses Don @ 2016-07-31
+#--------------------------------------------------------
+#
+# command:
+#   summary
+#
+
+new_card(title = 'Summary', c(
+  sprintf('Total Entries: %s', nrow(WORD_CACHE)),
+  sprintf('In scope: %s', sum(WORD_CACHE$in_lib) - sum(sapply(WORD_CACHE$word, is.pharse))),
+  sprintf('Pharse: %s', sum(sapply(WORD_CACHE$word, is.pharse))),
+  sprintf('Not in scope: %s', sum(!WORD_CACHE$in_lib))
+))
